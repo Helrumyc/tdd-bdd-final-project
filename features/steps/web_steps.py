9efb30b7d7b0ed185308817082ab75bf4d658ff8
Helrumyc <naw7961@rit.edu>
2025-04-04 16:45:02 -0400

    BDD test running, but need debugging

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -29,8 +29,12 @@ from behave import when, then
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import TimeoutException
 
 ID_PREFIX = 'product_'
+BUTTON_SUFFIX = '-btn'
+SEARCH_RESULTS = 'search_results'
+FLASH_MESSAGE = 'flash_message'
 
 
 @when('I visit the "Home Page"')
@@ -104,7 +108,10 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + BUTTON_SUFFIX
+    context.driver.find_element(By.ID, button_id).click()
 
 ##################################################################
 # This code works because of the following naming convention:
@@ -123,6 +130,34 @@ def step_impl(context, text_string, element_name):
         )
     )
     assert(found)
+
+@then('I should see "{name} in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, 60).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, SEARCH_RESULTS),
+            name
+        )
+    )
+    assert(found)
+
+@then('I should not see "{name} in the results')
+def step_impl(context, name):
+    element = context.driver.find_element(By.ID, SEARCH_RESULTS)
+    assert(name not in element.text)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    try:
+        found = WebDriverWait(context.driver, 60).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'flash_message'),
+                message
+            )
+        )
+        assert(found)
+    except TimeoutException:
+        raise AssertionError(f"Timeout: Expected message '{message}' not found within {context.wait_seconds} seconds.")
 
 @when('I change "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
